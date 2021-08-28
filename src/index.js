a555323a9a0e6cbc805aa252865d7d235e229845
nikeusov <nikotin331765115@gmail.com>
2021-08-28 14:54:15 +0300

    feat: add brackets func

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+  
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arr = [];
+  for (let elem of bracketsConfig) 
+  {
+    arr.push(elem.join(''))
+  }
+  
+  for (let i = 0; i < arr.length; i++) 
+  {
+    if (str.includes(arr[i])) 
+    {
+      str = str.replace(arr[i], '');
+      i =- 1
+    }
+  }
+ return str.length === 0;
 }
